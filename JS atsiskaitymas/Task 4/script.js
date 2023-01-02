4ba53f04d3c560c80635ba1bd7f4ce8355e663ef
ievaCode <iewute@yahoo.com>
2023-01-02 11:55:27 +0200

    4 task done with formating issues

--- JS atsiskaitymas/Task 4/script.js
+++ JS atsiskaitymas/Task 4/script.js
@@ -10,3 +10,48 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+
+fetch("./cars.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(brand => {
+      let newManufacturer = new Manufacturer(brand.brand, brand.models);
+      document.querySelector("#output").append(newManufacturer);
+    })    
+  });
+
+  class Manufacturer{
+    constructor(brand, models){
+      this.brand = brand;
+      this.models = models;
+      return this.render();
+    }
+    render = () => {
+      this.card = document.createElement('div');
+      this.card.classList.add('card');
+
+      this.paragraph1 = document.createElement("p");
+      this.paragraph1.classList.add('brand');
+      this.brandTextNode = document.createTextNode(`Brand: ${this.brand}`);
+      this.paragraph1.append(this.brandTextNode);
+
+      this.paragraph2 = document.createElement("p");
+      this.paragraph2.classList.add('explanatoryText');
+      this.explanatoryTextNode = document.createTextNode(`Models:`);
+      this.paragraph2.append(this.explanatoryTextNode);
+
+      this.listOfModels = document.createElement("ul");
+      this.listOfModels.classList.add('listOfModels');
+      this.models.forEach(model => {
+        this.li = document.createElement("li");
+        this.li.classList.add("modelName");
+        const modelTextNode = document.createTextNode(model);
+        this.li.append(modelTextNode);
+        this.listOfModels.append(this.li);
+      })
+      this.card.append(this.paragraph1, this.paragraph2, this.listOfModels);
+    
+      return this.card;
+    }
+  }
