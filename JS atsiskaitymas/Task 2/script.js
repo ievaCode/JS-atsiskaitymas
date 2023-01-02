845ab780d8f0e18eea1368a5396cb238355f26cf
ievaCode <iewute@yahoo.com>
2023-01-02 10:01:26 +0200

    2task done

--- JS atsiskaitymas/Task 2/script.js
+++ JS atsiskaitymas/Task 2/script.js
@@ -3,3 +3,10 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+let count = 0;
+document.querySelector("#btn__element")
+.addEventListener('click', (e) => 
+document.querySelector("#btn__state").textContent = count+=1
+);
+
