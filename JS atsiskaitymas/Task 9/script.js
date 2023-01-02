89667170c68dc392720119dbf5b7186d38071d84
ievaCode <iewute@yahoo.com>
2023-01-02 13:26:26 +0200

    task 9 done

--- JS atsiskaitymas/Task 9/script.js
+++ JS atsiskaitymas/Task 9/script.js
@@ -6,3 +6,15 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie{
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive(){
+    return this.budget > 100000000;
+  }
+}
+
