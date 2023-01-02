529b72c61f74ea0924deb9bb52f38ca8f3fb5c6d
ievaCode <iewute@yahoo.com>
2023-01-02 12:27:59 +0200

    task 6 done

--- JS atsiskaitymas/Task 6/script.js
+++ JS atsiskaitymas/Task 6/script.js
@@ -19,3 +19,16 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+let  getUserAverageAge = (data) => {
+  const sum = data.reduce((total, user) => total + user.age, 0);
+  return sum / users.length
+}
+
+let names = [];
+let  getUsersNames = (data) => {
+  data.forEach(user => names.push(user.name))
+  return names;
+}
+
+
